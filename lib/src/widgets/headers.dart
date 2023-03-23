5cc079e761f636a6538d1b9fbf5e8a4c6945bad5
Nacho <nacho_085@hotmail.com>
2023-03-23 10:11:02 +0100

    Initial commit

---
+++ lib/src/widgets/headers.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class HeaderCuadrado extends StatelessWidget {
+  const HeaderCuadrado({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
+
+class HeaderBordesRedondeados extends StatelessWidget {
+  const HeaderBordesRedondeados({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      decoration: const BoxDecoration(
+          color: Color(0xff615AAB),
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(70),
+            bottomRight: Radius.circular(70),
+          )),
+    );
+  }
+}
+
+class HeaderDiagonal extends StatelessWidget {
+  const HeaderDiagonal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 300,
+      color: Color(0xff615AAB),
+    );
+  }
+}
